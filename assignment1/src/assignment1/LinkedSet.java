61e98f4d4cfcdbc166129cfee5a6bc8e56e785cb
jai-rus <jairus.legion@gmail.com>
2023-10-14 20:01:38 -0700

    added linkedset and linkedsettest

---
+++ assignment1/src/assignment1/LinkedSet.java
@@ -0,0 +1,214 @@
+package assignment1;
+
+public class LinkedSet<T> implements SetInterface<T> {
+	//Node class
+	class Node {
+		private T data;
+		private Node next;
+		
+		//Constructors
+		private Node(T dataPortion) {
+			this(dataPortion, null);
+		}
+		
+		private Node(T dataPortion, Node nextNode) {
+			data = dataPortion;
+			next = nextNode;
+		}
+		
+		private T getData() {
+			return data;
+		}
+		
+		private void setData(T newData) {
+			data = newData;
+		}
+		
+		private Node getNextNode() {
+			return next;
+		}
+		
+		private void setNextNode(Node nextNode) {
+			next = nextNode;
+		}
+	}
+	
+	private Node firstNode;
+	private int numOfEntries;
+	
+	//Constructor
+	public LinkedSet() {
+		firstNode = null;
+		numOfEntries = 0;
+	}
+	
+	//adds a new entry
+	public boolean add(T newEntry) {
+		if (numOfEntries == 0) {
+			Node newNode = new Node(newEntry);
+			firstNode = newNode;
+			numOfEntries++;
+			//System.out.println("Added: " + firstNode.getData());
+			return true;
+		} else {
+			Node newNode = new Node(newEntry);
+			newNode.setNextNode(firstNode);
+			firstNode = newNode;
+			numOfEntries++;
+			//System.out.println("Added: " + firstNode.getData());
+			return true;
+		}
+	}
+	
+	//removes the first node
+	public T remove() {
+		if (numOfEntries == 0) {
+			return null;
+		} else {
+			T temp = firstNode.getData();
+			firstNode = firstNode.getNextNode();
+			numOfEntries--;
+			return temp;
+		}
+	}
+	
+	//removes specific node
+	public boolean remove(T anEntry) {
+		boolean result = false;
+		Node nodeN = getReferenceTo(anEntry);
+		System.out.println(nodeN.getData());
+		//sets firstNode data to the removedNode
+		//removes firstNode
+		if (nodeN != null) {
+			nodeN.setData(firstNode.getData());
+			firstNode = firstNode.getNextNode();
+			numOfEntries--;
+			
+			result = true;
+		}
+		
+		return result;
+	}
+	
+	//finds a specific entry and returns the node
+	private Node getReferenceTo(T anEntry) {
+		boolean found = false;
+		Node currentNode = firstNode;
+		
+		//while its not found and curretNode isn't null
+		while (!found && (currentNode != null)) {
+			
+			if (anEntry.equals(currentNode.getData())) {
+				found = true;
+			} else {
+				currentNode = currentNode.getNextNode();
+			}
+		}
+		
+		return currentNode;
+	}
+	
+	//gets current size of set
+	public int getCurrentSize() {
+		return numOfEntries;
+	}
+	
+	//checks if set is empty
+	public boolean isEmpty() {
+		if (numOfEntries == 0) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	//removes everything in the set
+	public void clear() {
+		while (!isEmpty()) {
+			remove();
+		}
+	}
+	
+	//check if the set contains an entry
+	public boolean contains(T anEntry) {
+		boolean found = false;
+		Node curNode = firstNode;
+		
+		while(!found && (curNode != null)) {
+			if (anEntry.equals(curNode.getData())) {
+				found = true;
+				break;
+			} else {
+				curNode = curNode.getNextNode();
+			}
+		}
+		
+		return found;
+	}
+	
+	//turns the set into an array
+	public T[] toArray() {
+		@SuppressWarnings("unchecked")
+		T[] result = (T[]) new Object[numOfEntries];
+		
+		int index = 0;
+		Node curNode = firstNode;
+		
+		while((index < numOfEntries) && (curNode != null)) {
+			result[index] = curNode.getData();
+			index++;
+			curNode = curNode.getNextNode();
+		}
+		
+		return result;
+	}
+
+	//gets a union of curSet and otherSet
+	public SetInterface<T> union(SetInterface<T> otherSet) {
+		Node curNode = firstNode;
+		
+		SetInterface<T> everything = new LinkedSet<T>();
+		
+		//add everything from the current node to the new set
+		while (curNode != null) {
+			everything.add(curNode.getData());
+			//System.out.println(curNode.getData());
+			curNode = curNode.getNextNode();
+		}
+		
+		T[] otherSetArray= otherSet.toArray();
+
+		for (int i = 0; i < otherSet.getCurrentSize(); i++) {
+			if (!contains(otherSetArray[i])) { //if curSet doesn't contain the current element add it
+				everything.add(otherSetArray[i]);
+				//System.out.println(otherSetArray[i]);
+			}
+		}
+		
+		return everything;
+	}
+	
+	public SetInterface<T> intersection(SetInterface<T> otherSet) {
+		Node curNode = firstNode;
+		int length = 0;
+		
+		//set length to the biggest set
+		if (numOfEntries < otherSet.getCurrentSize()) {
+			length = otherSet.getCurrentSize();
+		} else {
+			length = numOfEntries;
+		}
+		
+		SetInterface<T> commonItems = new LinkedSet<T>();
+		
+		T[] otherSetArray= otherSet.toArray();
+		
+		for (int i = 0; i < length; i++) {
+			if(contains(otherSetArray[i]) && (!commonItems.contains(otherSetArray[i]))) {
+				commonItems.add(otherSetArray[i]);
+			}
+		}
+		
+		return commonItems;
+	}
+}
---
+++ assignment1/src/assignment1/LinkedSetTest.java
@@ -0,0 +1,109 @@
+package assignment1;
+
+public class LinkedSetTest {
+	public static void main(String[] args) {
+		SetInterface<String> set1 = new LinkedSet<>();
+		
+		System.out.println("Using Linked Set");
+		//Uses isEmpty()
+		System.out.println("Is empty = " + set1.isEmpty());
+		
+		//Uses add()
+		set1.add("A");
+		set1.add("B");
+		set1.add("C");
+		
+		//Uses getCurretnSize()
+		System.out.println("Current size is " + set1.getCurrentSize());
+		System.out.println("Is empty = " + set1.isEmpty()); //Uses isEmpty();
+		
+		//Uses remove()
+		System.out.println("Removed " + set1.remove());
+		set1.add("D");
+		
+		//Uses remove(anEntry)
+		System.out.println("Removing B: " + set1.remove("B"));
+		
+		//Uses clear()
+		set1.clear();
+		System.out.println("Is empty = " + set1.isEmpty());
+		
+		set1.add("A");
+		set1.add("B");
+		set1.add("C");
+		
+		//Uses contains()
+		System.out.println("Does this set contain A: " + set1.contains("A"));
+		System.out.println("Does this set contain Z: " + set1.contains("Z"));
+		
+		//Uses toArray()
+		Object[] testArray = new Object[3]; 	
+		testArray = set1.toArray();
+		for (int i = 0; i < 3; i++) {
+			System.out.print(testArray[i] + " ");
+		}
+		
+		System.out.println();
+		
+		SetInterface<String> set2 = new LinkedSet<>();
+		set2.add("A");
+		set2.add("B");
+		set2.add("C");		
+		
+		//testing union
+		SetInterface<String> everything = set1.union(set2);
+		Object[] testArray2 = new Object[everything.getCurrentSize()];
+		testArray2 = everything.toArray();
+		System.out.println("Union: ");
+		for (int i = 0; i < testArray2.length; i++) {
+			System.out.print(testArray2[i] + " ");
+		}
+		
+		System.out.println();
+		
+		//testing intersection
+		set1.add("D");
+		set2.add("A");
+		SetInterface<String> commonItems = set1.intersection(set2);
+		
+		Object[] testArray3 = new Object[commonItems.getCurrentSize()];
+		testArray3 = commonItems.toArray();
+		System.out.println("Intersection: ");
+		for (int i = 0; i < testArray3.length; i++) {
+			System.out.print(testArray3[i] + " ");
+		}
+		
+		System.out.println();
+		/*
+		//testing difference
+		SetInterface<String> set3 = new ResizeableArraySet<>();
+		set3.add("A");
+		set3.add("B");
+		set3.add("C");
+		
+		SetInterface<String> set4 = new ResizeableArraySet<>();
+		set4.add("B");
+		set4.add("C");
+		set4.add("D");
+		set4.add("E");
+		
+		SetInterface<String> leftOver1 = set3.difference(set4);
+		Object[] testArray4 = new Object[leftOver1.getCurrentSize()];
+		testArray4 = leftOver1.toArray();
+		System.out.println("Difference: ");
+		for (int i = 0; i < testArray4.length; i++) {
+			System.out.print(testArray4[i] + " ");
+		}
+		
+		System.out.println();
+		
+		SetInterface<String> leftOver2 = set4.difference(set3);
+		Object[] testArray5 = new Object[leftOver2.getCurrentSize()];
+		testArray5 = leftOver2.toArray();
+		System.out.println("Difference: ");
+		for (int i = 0; i < testArray5.length; i++) {
+			System.out.print(testArray5[i] + " ");
+		}
+		*/
+	}
+}
